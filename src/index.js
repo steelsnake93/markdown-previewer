45cbe52a12cdbca97e2d61f9a04ea2af1786ba4b
steelsnake93 <nikolanikotasev@protonmail.com>
2023-03-03 15:39:18 +0100

    Add initial code for markdown previewer, add styling for markdown previewer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const myFirstElement = <h1>Hello!</h1>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
